c8923e5772b02aef956ba57ff100eb6f7998e3ae
Khairul Fikri <48195138+keruliki@users.noreply.github.com>
2022-11-28 22:39:46 +0800

    color changes based on match status

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
+import React from "react";
 
 export const Scoreboard = (props) => {
-
   return (
-    <div className='grid grid-cols-6 my-2'>
+    <div className={props.value.status == 'FT' ? "bg-green-900 p-4 my-5 rounded-lg" : "bg-red-900 p-4 my-5 rounded-lg"}>
+      <div className="grid grid-cols-6 my-2">
         <div>
-            <small>{props.value.status}</small>
+          <small>{props.value.status}</small>
         </div>
-        <div className='col-span-2'>
-            <h1 className='text-lg font-bold'>{props.value.home}</h1>
+        <div className="col-span-2">
+          <h1 className="text-lg font-bold">{props.value.home}</h1>
         </div>
-        <div id='score' className='text-center'>
-            <p>{props.value.homeScore} - {props.value.awayScore}</p>
+        <div id="score" className="text-center">
+          <p>
+            {props.value.homeScore} - {props.value.awayScore}
+          </p>
         </div>
-        <div className='col-span-2 text-right'>
-            <h1 className='text-lg font-bold'>{props.value.away}</h1>
+        <div className="col-span-2 text-right">
+          <h1 className="text-lg font-bold">{props.value.away}</h1>
         </div>
-
+      </div>
     </div>
-  )
-}
+  );
+};
