8001328b35333437cc20e4f4dbf5a4808c9627f8
Khairul Fikri <48195138+keruliki@users.noreply.github.com>
2022-11-28 21:58:59 +0800

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-gray-800 text-white py-6 px-4 text-center border-b-2 border-white'>
+        <h1 className='text-2xl font-bold'>My LiveScore</h1>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/page/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+
+function Home() {
+  return (
+    <div className="bg-gray-800 text-white h-[100vh]">
+      <Navbar/>
+      <h1>Home</h1>
+    </div>
+  )
+}
+
+export default Home
